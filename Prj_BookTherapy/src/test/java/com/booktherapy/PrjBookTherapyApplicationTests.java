df5a83d0a606ec29d6edebc5e52f20e3a52b47c0
giadaperno <giada.perno@edu-its.it>
2025-05-22 11:07:45 +0200

    a caso

---
+++ Prj_BookTherapy/src/test/java/com/booktherapy/PrjBookTherapyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booktherapy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjBookTherapyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
