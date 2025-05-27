abd2b60152bde4f547add18b104eff36ee56a1cf
giadaperno <giada.perno@edu-its.it>
2025-05-27 15:27:44 +0200

    Credenziali rese private

--- Prj_BookTherapy/src/main/java/repos/Connessione.java
+++ Prj_BookTherapy/src/main/java/repos/Connessione.java
@@ -1,22 +1,29 @@
 package repos;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+@Component
 public class Connessione {
 
     private final String HOST = "jdbc:mysql://letturaplatform-database2025.d.aivencloud.com:19066/defaultdb?sslMode=REQUIRED";
-    private final String USER = "";
-    private final String PASS = "";
+
+    @Value("${spring.datasource.username}")
+    private String USER;
+
+    @Value("${spring.datasource.password}")
+    private String PASS;
 
     private Connection conn;
 
     private void connetti() {
         try {
-        	
             System.setProperty("javax.net.ssl.trustStore", "src/main/resources/keystore.jks");
-            System.setProperty("javax.net.ssl.trustStorePassword", "changeit"); 
+            System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
 
             this.conn = DriverManager.getConnection(HOST, USER, PASS);
             System.out.println("Connesso al DB Aiven");
@@ -31,5 +38,4 @@ public class Connessione {
         if (conn == null) connetti();
         return this.conn;
     }
-}
-
+    }
