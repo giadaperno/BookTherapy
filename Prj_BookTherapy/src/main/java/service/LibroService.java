65220b62409cb05551bb28ae2ba338eb367e4fd4
giadaperno <giada.perno@edu-its.it>
2025-06-12 12:55:37 +0200

    Feature/giovene (#17)
    * primo commit
    
    * Create Libro.java
    
    * Update Connessione.class
    
    * Update Libro.java
    
    * Update Libro.java
    
    * Aggiunta classe libro alle entity
    
    ho aggiunto il package entities e la classe Libro come Entity per collagarla con il DB
    
    * Aggiunta classe libro alle entity
    
    ho aggiunto il package entities e la classe Libro come Entity per collagarla con il DB
    
    * modificata classe Libro
    
    * Updated XML http to https URLs in pom.xml for consistency
    
    * Correzione classe Libro tipo Long per l'id e aggiunta interfaccia LibroRepository con metodi di query per il filtraggio dei libri
    
    * aggiunto @Column(name = "id_libro") per far combaciare id su java e id_libro su DB
    
    * mi sono messo in pari con il main
    
    * alliniamento con Giada
    
    * aggiunta LibroService
    
    ---------
    
    Co-authored-by: Andrea Giovene <andrea.giovene@edu-its.it>

---
+++ Prj_BookTherapy/src/main/java/service/LibroService.java
@@ -0,0 +1,47 @@
+package service;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import entities.Libro;
+import repos.LibroRepository;
+
+@Service // Indica a Spring che questa è una classe di servizio
+public class LibroService {
+
+    private final LibroRepository libroRepository;
+
+    @Autowired // Inietta automaticamente un'istanza di LibroRepository
+    public LibroService(LibroRepository libroRepository) {
+        this.libroRepository = libroRepository;
+    }
+
+    // Cerca libri per titolo
+    public List<Libro> cercaLibriPerTitolo(String titolo) {
+        return libroRepository.findByTitoloContainingIgnoreCase(titolo);
+    }
+
+    // Cerca libri per autore
+    public List<Libro> cercaLibriPerAutore(String autore) {
+        return libroRepository.findByAutoreContainingIgnoreCase(autore);
+    }
+
+    // Cerca libri con un prezzo massimo
+    public List<Libro> cercaLibriPerPrezzoMassimo(BigDecimal prezzoMax) {
+        return libroRepository.findByPrezzoLessThanEqual(prezzoMax);
+    }
+
+    // Cerca un libro per ISBN
+    public Optional<Libro> cercaLibroPerIsbn(String isbn) {
+        return libroRepository.findByIsbn(isbn);
+    }
+
+    // Cerca un libro per il suo ID univoco
+    public Optional<Libro> cercaLibroPerId(Long id) {
+        return libroRepository.findById(id);
+ 
+    }
+}
